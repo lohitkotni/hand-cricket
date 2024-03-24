1cbd03f5eb1283f813a092fe85fa7e0cc18b497a
Lohith Kotni <lohitkotni@gmail.com>
2024-03-24 19:37:33 +0530

    fixed bugs

--- app/toss-result/route.tsx
+++ app/toss-result/route.tsx
@@ -1,4 +1,3 @@
-
 import { createFrames, Button } from "frames.js/next";
 
 const frames = createFrames();
@@ -6,51 +5,58 @@ const frames = createFrames();
 const handleRequest = frames(async (ctx) => {
   const randomNumber = Math.floor(Math.random() * 2) + 1;
   const computerChoice = Math.floor(Math.random() * 2) + 1;
-  let result, buttons;
 
   if (randomNumber === ctx.pressedButton?.index) {
-    result = "You won the toss";
-    buttons = [
-      <Button action="post" target={{ query: { choice: "batting", userScore:0 },pathname:"/userBat" }}>
-        Batting
-      </Button>,
-      <Button action="post" target={{ query: { choice: "bowling" },pathname:"/userBowl"}}>
-        Bowling
-      </Button>,
-    ];
+    return {
+      image: (
+        <div tw="bg-pink-200 w-screen h-screen flex justify-center items-center">
+          You won the toss
+        </div>
+      ),
+      buttons: [
+        <Button action="post" target={{ query: { choice: "batting", userScore:0 },pathname:"/userBat" }}>
+          Batting
+        </Button>,
+        <Button action="post" target={{ query: { choice: "bowling" },pathname:"/userBowl"}}>
+          Bowling
+        </Button>,
+      ],
+    };
   } else {
-    result = `You lost the toss and computer chose to ${
-      computerChoice === 1 ? "bat" : "bowl"
-    }`;
-    if (result == "You lost the toss and computer chose to bat") {
-      buttons = [
-        <Button
-          action="post"
-          target={{ query: { choice: "continueBowl" }, pathname :"/userBowl" }}
-        >
-          Continue
-        </Button>,
-      ];
+    if (Math.floor(Math.random()*2) === 0) {
+      return {
+        image: (
+          <div tw="bg-pink-200 w-screen h-screen flex justify-center items-center">
+            You lost the toss and computer chose to bat
+          </div>
+        ),
+        buttons: [
+          <Button
+            action="post"
+            target={{ query: { choice: "continueBowl" }, pathname :"/userBowl" }}
+          >
+            Continue
+          </Button>,
+        ]
+      };
     } else {
-      buttons = [
-        <Button
-          action="post"
-          target={{ query: { choice: "continueBat", userScore:0 }, pathname:"/userBat" }}
-        >
-          Continue
-        </Button>,
-      ];
+      return {
+        image: (
+          <div tw="bg-pink-200 w-screen h-screen flex justify-center items-center">
+            You lost the toss and computer chose to bowl
+          </div>
+        ),
+        buttons: [
+          <Button
+            action="post"
+            target={{ query: { choice: "continueBat", userScore:0 }, pathname:"/userBat" }}
+          >
+            Continue
+          </Button>,
+        ]
+      };
     }
   }
-
-  return {
-    image: (
-      <div tw="bg-pink-200 w-screen h-screen flex justify-center items-center">
-        {result}
-      </div>
-    ),
-    buttons: buttons,
-  };
 });
 
 export const GET = handleRequest;
